f1eba0964d5e4c5d93f658f6276fa6cba0a4dd16
Tracey Kadenyi <treykadenyi@gmail.com>
2026-05-26 17:07:47 +0300

    index.js

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const { WebSocketServer } = require("ws");
+
+const server = http.createServer();
+const wsServer = new WebSocketServer({ server });
+const port = 8000;
+
+server.listen(port, () => {
+  console.log(`WebSocket server is running on port ${port}`);
+});
