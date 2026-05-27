e8f5d3daedb08706af249e4c64c3c4fbdcef2b49
Tracey Kadenyi <treykadenyi@gmail.com>
2026-05-27 19:34:36 +0300

    handle server connections

--- server/index.js
+++ server/index.js
@@ -1,9 +1,62 @@
 const http = require("http");
 const { WebSocketServer } = require("ws");
 
+const url = require("url");
+const uuidv4 = require("uuid").v4;
+
 const server = http.createServer();
 const wsServer = new WebSocketServer({ server });
 const port = 8000;
+
+const connections = {};
+const users = {};
+
+const broadcast = () => {
+  Object.keys(connections).forEach((uuid) => {
+    const connection = connections[uuid];
+    const message = JSON.stringify(users);
+    connection.send(message);
+  });
+};
+
+// message = state
+const handleMessage = (bytes, uuid) => {
+  const message = JSON.parse(bytes.toString());
+  const user = users[uuid];
+  user.state = message;
+  console.log(
+    `${user.username} updated their state: ${JSON.stringify(user.state)}`,
+  );
+
+  broadcast();
+};
+
+const handleClose = (uuid) => {
+  console.log(`${users[uuid].username} disconnected`);
+  delete connections[uuid];
+  delete users[uuid];
+
+  broadcast();
+};
+
+wsServer.on("connection", (connection, request) => {
+  // ws://localhost:8000?username=Alex
+
+  const { username } = url.parse(request.url, true).query;
+  const uuid = uuidv4();
+  console.log(username);
+  console.log(uuid);
+
+  connections[uuid] = connection;
+
+  users[uuid] = {
+    username,
+    state: {},
+  };
+
+  connection.on("message", (message) => handleMessage(message, uuid));
+  connection.on("close", () => handleClose(uuid));
+});
 
 server.listen(port, () => {
   console.log(`WebSocket server is running on port ${port}`);
